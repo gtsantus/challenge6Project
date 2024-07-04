2bebc650af548903423897043a5b0ca420b870bf
Gwen <gt.santus@gmail.com>
2024-07-04 13:55:35 +0100

    Front end testing done

--- Challenge6React/src/components/Header.jsx
+++ Challenge6React/src/components/Header.jsx
@@ -1,7 +1,8 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import authService from "../services/auth.service";
 import logo from "./images/Card Logo.jpg";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 
 const Header = ({ currentUser, setCurrentUser, showAdminContent }) => {
   const loggedIn = Boolean(currentUser);
@@ -18,53 +19,37 @@ const Header = ({ currentUser, setCurrentUser, showAdminContent }) => {
   };
 
   return (
-    <div>
-      <header
-        className="d-flex justify-content-between align-items-center p-3 text-white"
-        style={{ backgroundColor: "#001450", color: "white" }}
-      >
-        <div className="d-flex align-items-center">
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
           <img
             src={logo}
             alt="Logo"
-            className="me-5"
             style={{ width: "75px", height: "75px" }}
           />
-          <Link to="/" className="me-5">
-            Home
-          </Link>
-          <Link to="/ViewDecks" className="me-5">
-            My Decks
-          </Link>
-          <Link to="/ViewCards" className="me-5">
-            All Cards
-          </Link>
-          {showAdminContent && (
-            <div>
-              <Link to="/AddCard">Add Card</Link>
-            </div>
-          )}
-        </div>
-        {loggedIn && (
-          <div>
-            <button type="button" className="btn btn-light" onClick={logOut}>
-              Log Out
-            </button>
-          </div>
-        )}
-        {!loggedIn && (
-          <div>
-            <button
-              type="button"
-              className="btn btn-light"
-              onClick={navigateToLogin}
-            >
-              Login
-            </button>
-          </div>
-        )}
-      </header>
-    </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/ViewDecks">My Decks</Nav.Link>
+            <Nav.Link href="/ViewCards">All Cards</Nav.Link>
+            {showAdminContent && <Nav.Link href="/AddCard">Add Card</Nav.Link>}
+          </Nav>
+          <Nav>
+            {loggedIn ? (
+              <Button variant="light" onClick={logOut}>
+                Log Out
+              </Button>
+            ) : (
+              <Button variant="light" onClick={navigateToLogin}>
+                Login
+              </Button>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
